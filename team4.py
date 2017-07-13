e1f64dc17d0648a35a5af4435badcc69a6e4b9b4
Ddilrukshi <Ddilrukshi@taehs.org>
2017-07-13 08:09:38 -0400

    Made team 4 changes

--- team4.py
+++ team4.py
@@ -6,29 +6,22 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'DSM' # Only 10 chars displayed.
+strategy_name = 'Revengers'
+strategy_description = 'If they have ever betrayed, we are out to get them and if our score is low we will betray'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
-    
+    if my_history <2:
+        return'c'
+                
+    if 'b' in their_history: 
+        return 'b'
+    if my_score >=-500:
+        return 'b'          
+    else:
+        return 'c'
+        
+                    
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
