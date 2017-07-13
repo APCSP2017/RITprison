d92f41bcd9676081929da35c06373ee734ba2c11
HGP Local Machine <bpetcaugh@gmail.com>
2017-07-13 08:20:42 -0400

    Made team 8 changes.

--- team8.py
+++ team8.py
@@ -6,27 +6,31 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Petcaugh' # Only 10 chars displayed.
+strategy_name = 'Percentage Destruction'
+strategy_description = 'Betray based on a percent'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+            opp_b = 0
+            opp_c = 0
+            if len(their_history)<10: #It's the first round: collude
+                return 'c'
+            elif len(their_history)<20:
+                return 'b'
+            else:
+                for choice in their_history:
+                    if choice == 'b':
+                        opp_b += 1
+                    else:
+                        opp_c += 1
+                if opp_c != 0:
+                    percentage = float(opp_b / opp_c)
+                else:
+                    return 'b'
+                if percentage >= 1:
+                    return 'b'
+                else:
+                    return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
