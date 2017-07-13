ce6dd21c067ca3fb72e6b8069fcf8514fdda22c8
cmorales1966 <cmorales1966@users.noreply.github.com>
2017-07-13 08:08:49 -0400

    made team 3 changes

--- team3.py
+++ team3.py
@@ -1,3 +1,4 @@
+import random
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -6,17 +7,20 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Terminator' # Only 10 chars displayed.
+strategy_name = 'Randomnizer'
+strategy_description = 'randomly collude or betray!'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
     '''
+    action = random.choice([0,1])
+    if action==0:
+        return 'c' #Collude
+    else:
+        return 'b' #Betray       
+  
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
